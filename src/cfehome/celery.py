e24ac864ab7ded8e1da80ebd3654ef87216d59ba
CodingForEntrepreneurs <hello@teamcfe.com>
2024-07-08 16:28:45 -0600

    12 - Auto Schedule Tasks with Celery Beat

--- src/cfehome/celery.py
+++ src/cfehome/celery.py
@@ -6,3 +6,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
 app = Celery("cfehome")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+CELERY_BEAT_SCHEDULE = {
+    "check-temp": {
+        "task": "sensors.tasks.measure_temp_task",
+        "schedule": 5.0 # every 5 seconds
+    },
+}
+
+# celery -A cfehome worker --beat -l info
+app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
