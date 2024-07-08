bcd6ddda60f223e9fa3ff8adf184c975dc1d7caa
CodingForEntrepreneurs <hello@teamcfe.com>
2024-07-08 17:08:18 -0600

    13 - Celery Task Queues for Individual Worker Nodes

--- src/cfehome/celery.py
+++ src/cfehome/celery.py
@@ -1,18 +1,34 @@
 import os
 
 from celery import Celery
+from kombu import Queue, Exchange
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
 
 app = Celery("cfehome")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
-CELERY_BEAT_SCHEDULE = {
-    "check-temp": {
-        "task": "sensors.tasks.measure_temp_task",
-        "schedule": 5.0 # every 5 seconds
-    },
-}
+nodes = [
+    "node-1",
+    "node-2",
+    "node-3",
+]
 
-# celery -A cfehome worker --beat -l info
+CELERY_TASK_QUEUES = []
+CELERY_BEAT_SCHEDULE = {}
+for node in nodes:
+    CELERY_TASK_QUEUES.append(
+        Queue(node, Exchange(node), routing_key=node),
+    )
+    key = f"check-temp-{node}"
+    CELERY_BEAT_SCHEDULE[key] = {
+        "task": "sensors.tasks.measure_temp_task",
+        "schedule": 5.0, # every 5 seconds
+        "options": {"queue": node} # apply_async(queue="node-1")
+    }
+
+app.conf.task_queues = CELERY_TASK_QUEUES
+# celery -A cfehome worker -Q node-2 -l info
+# celery -A cfehome beat -l info
 app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
