cce05132a346923f869f1d76494f97f2d7f5831a
CodingForEntrepreneurs <hello@teamcfe.com>
2024-07-08 17:47:19 -0600

    14 - Django Project within Docker Compose

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
