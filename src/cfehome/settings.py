209e29db1b3597e29c8e02af09a2484e5c8029fe
CodingForEntrepreneurs <hello@teamcfe.com>
2024-07-09 15:55:52 -0600

    23 - Visualizing Average Temperature over time

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "cfehome.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 from .api import api
+from . import views 
 
 urlpatterns = [
+    path("", views.home_view),
     path("admin/", admin.site.urls),
     path("api/", api.urls)
 ]
