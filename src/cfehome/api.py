c29bd30736781d4ebe29c89d3cc1f63a02b6170d
CodingForEntrepreneurs <hello@teamcfe.com>
2024-07-09 15:25:53 -0600

    22 - Additional Database Queries

--- src/cfehome/api.py
+++ src/cfehome/api.py
@@ -7,11 +7,46 @@ from sensors import services
 
 api = NinjaAPI()
 
+
+class NodeMaxMinTempSchema(Schema): # pydantic
+    node_id: int
+    max_temp: float
+    min_temp: float
+
+
+class MaxMinTempSchema(Schema): # pydantic
+    max_temp: float
+    min_temp: float
+
+
 class AvgTempSchema(Schema): # pydantic
+    avg_temp: float
+    time_group: datetime
+
+
+class NodeAvgTempSchema(Schema): # pydantic
+    node_id: int
     avg_temp: float
     time_group: datetime
 
 @api.get('/temps', response=List[AvgTempSchema])
 def get_average_temps(request):
     qs = services.get_avg_temp()
+    return qs
+
+
+@api.get('/temps/node', response=List[NodeAvgTempSchema])
+def get_average_temps(request):
+    qs = services.get_node_avg_temp()
+    return qs
+
+
+@api.get('/temps/maxmin', response=MaxMinTempSchema)
+def get_average_temps(request):
+    qs = services.get_max_min_temp()
+    return qs
+
+@api.get('/temps/nodes/maxmin', response=List[NodeMaxMinTempSchema])
+def get_average_temps(request):
+    qs = services.get_node_max_min_temp()
     return qs
--- src/sensors/services.py
+++ src/sensors/services.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime
-from django.db.models import Avg
+from django.db.models import Avg, Max, Min
 from django.db.models.functions import TruncMinute
 from django.utils import timezone
 from .models import Metric
@@ -14,5 +14,42 @@ def get_avg_temp():
         .values('time_group')
         .annotate(avg_temp=Avg('temperature'))
         .order_by('time_group')
+    )
+    return qs
+
+
+def get_node_avg_temp():
+    now = timezone.now()
+    start_range = (now-timedelta(days=1))
+    qs = (
+        Metric.timescale
+        .filter(time__range=(start_range, now))
+        .annotate(time_group=TruncMinute('time'))
+        .values('time_group', 'node_id')
+        .annotate(avg_temp=Avg('temperature'))
+        .order_by('time_group', 'node_id')
+    )
+    return qs
+
+
+def get_max_min_temp():
+    now = timezone.now()
+    start_range = (now-timedelta(days=1))
+    qs = (
+        Metric.timescale
+        .filter(time__range=(start_range, now))
+        .aggregate(max_temp=Max('temperature'), min_temp=Min("temperature"))
+    )
+    return qs
+
+def get_node_max_min_temp():
+    now = timezone.now()
+    start_range = (now-timedelta(days=1))
+    qs = (
+        Metric.timescale
+        .filter(time__range=(start_range, now))
+        .values("node_id")
+        .annotate(max_temp=Max('temperature'), min_temp=Min("temperature"))
+        .order_by('node_id')
     )
     return qs
